e5566029f39646f7bf0c3d8a90092310b8ed512a
Raimond Fowler <raifowler@gmail.com>
2018-08-13 15:30:03 -0700

    Added firebase to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as firebase from 'firebase';
+
+<script src="https://www.gstatic.com/firebasejs/5.3.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAh6ex4AolBTjLnj6tTKxNyghdeJKSMTWI",
+    authDomain: "bloc-chat-962f1.firebaseapp.com",
+    databaseURL: "https://bloc-chat-962f1.firebaseio.com",
+    projectId: "bloc-chat-962f1",
+    storageBucket: "bloc-chat-962f1.appspot.com",
+    messagingSenderId: "285962152147"
+  };
+  firebase.initializeApp(config);
+</script>
+
 class App extends Component {
   render() {
     return (
