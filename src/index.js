a54de581b9b073713143da492643f65b4e79f8c6
Raimond Fowler <raifowler@gmail.com>
2018-08-14 18:42:19 -0700

    Added popper.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
